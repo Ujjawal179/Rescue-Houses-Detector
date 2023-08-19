175ef97635eed8a51dc8b346a4cd23756440d951
Ujjawal Agrawal <72138726+Ujjawal179@users.noreply.github.com>
2023-08-19 14:43:38 +0530

    Update main.py
    added the working of get_info function for further use

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from imagegenerator import imagegen
+import imagegenerator as img
 
-image = imagegen(r'round2/uas/images/image1.png')
+image = (img.imagegen(r'round2/uas/images/image1.png'))
+unburnt_red, unburnt_blue = img.get_info(image, (250,252,83))
+burnt_red, burnt_blue = img.get_info(image, (134,251,252))
+print(burnt_red, burnt_blue )
+print(unburnt_red, unburnt_blue )
